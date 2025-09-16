c42b6649c37ffc4ba7f9c0a8c4ac75c30b46d53f
pedroetec1930 <pedrodesenegouveia@gmail.com>
2025-09-16 10:39:22 -0300

    'finalizacao'

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,42 +1,67 @@
-import { useEffect, useState} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
-import {FaTrashAlt, FaCircle} from 'react-icons/fa'
+import { FaTrashAlt, FaCircle } from 'react-icons/fa'
 import api from '../../services/api'
 
 
 function Home() {
   const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
 
-  async function getUsuarios(){
+  async function getUsers() {
     const usersFromApi = await api.get('/users')
     setUsers(usersFromApi.data)
+  }
 
+  async function createUser() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: Number(inputAge.current.value),
+      email: inputEmail.current.value
+    })
+    getUsers()
+  }
+
+  async function editUser(id) {
+    await api.put(`/users/${id}`, {
+      name: inputName.current.value,
+      age: Number(inputAge.current.value),
+      email: inputEmail.current.value
+    })
+    getUsers()
+  }
+
+  async function delUser(id) {
+    await api.delete(`/users/${id}`)
+    getUsers()
   }
 
   useEffect(() => {
-    getUsuarios()
+    getUsers()
   }, [])
 
   return (
     <div className="container">
       <form>
-        <h1>Cadastro de Usuários</h1>
-        <input name='name' type='text' placeholder='Name' />
-        <input name='age' type='number' placeholder='Age' />
-        <input name='email' type='email' placeholder='Email' />
-        <button type="button">Sign In</button>
+        <h1>Sign In</h1>
+        <input name='name' type='text' placeholder='Name' ref={inputName} />
+        <input name='age' type='number' placeholder='Age' ref={inputAge} />
+        <input name='email' type='email' placeholder='Email' ref={inputEmail} />
+        <button type="button" onClick={createUser}>Sign In</button>
       </form>
 
       {users.map(user => (
         <div key={user.id} className='card'>
-        <div>
-          <p>Nome: {user.name}</p>
-          <p>Age: {user.age}</p>
-          <p>Email: {user.email}</p>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button onClick={()=> editUser(user.id)}><FaCircle /></button>
+          <button onClick={()=> delUser(user.id)}><FaTrashAlt /></button>
         </div>
-        <button><FaCircle/></button>
-        <button><FaTrashAlt/></button>
-      </div>
       ))}
     </div>
   )
